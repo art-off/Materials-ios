0c80f08ae84a67a01b3816770e122837604a1aea
Artem Rylov <tema2707@icloud.com>
2020-07-22 16:51:22 +0700

    Initial commit 2

---
+++ Materials/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Materials
+//
+//  Created by art-off on 22.07.2020.
+//  Copyright © 2020 art-off. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
